9e392ef058771fa8e866e8f79ad73d338a0a3ae0
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-28 20:41:40 -0500

    Update images and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: "https://me.jesubohrdev.com",
+  integrations: [tailwind(), robotsTxt()]
 })
